c84ef6f18640de0d8d2a91346cf4de0039e9975b
Supawitsaelim <oatsupawit_159@outlook.com>
2023-10-14 16:34:07 +0700

    up

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
-import paramiko
-import time 
+from netmiko import ConnectHandler
 
 app = Flask(__name__)
 
-# Create an SSH client
-ssh_client = paramiko.SSHClient()
-ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+cisco_device = {
+    'device_type': 'cisco_ios',
+    'ip': '10.4.15.29',
+    'username': 'admin',
+    'password': 'admin',
+    'session_log': 'output.log',  # Specify a log file
+}
+
 
 @app.route('/')
 def index():
@@ -22,8 +26,6 @@ def login():
     else:
         return '<script>alert("Incorrect username or password!!"); window.location.href="/";</script>'
 
-
-
 @app.route('/ssh', methods=['GET', 'POST'])
 def ssh():
     if request.method == 'POST':
@@ -31,33 +33,38 @@ def ssh():
         ssh_username = request.form.get('ssh_username')
         ssh_password = request.form.get('ssh_password')
         hostname = request.form.get('hostname')
-        
-        try:
-            # Establish SSH connection
-            ssh_client.connect(ip_address, port=22, username=ssh_username, password=ssh_password, allow_agent=False, look_for_keys=False)
-            time.sleep(5)
-            print("Successfully connected to " + ip_address)
 
+        # Update the cisco_device dictionary with the form input
+        cisco_device['ip'] = ip_address
+        cisco_device['username'] = ssh_username
+        cisco_device['password'] = ssh_password
+
+        try:
+            # Connect to the Cisco device
+            net_connect = ConnectHandler(**cisco_device)
+            net_connect.enable()
 
             if hostname != '':
-                # Execute command
-                stdin, stdout, stderr = ssh_client.exec_command('enable')
-                stdin, stdout, stderr = ssh_client.exec_command('admin')
-                stdin, stdout, stderr = ssh_client.exec_command('conf t')
-                stdin, stdout, stderr = ssh_client.exec_command('hostname ' + hostname)
-                stdin, stdout, stderr = ssh_client.exec_command('end')
-                output = stdout.readlines()
+                # Execute configuration commands
+                config_commands = [
+                    'enable',
+                    'admin',
+                    'configure terminal',
+                    f'hostname {hostname}',
+                    'end',
+                ]
+                output = net_connect.send_config_set(config_commands)
                 print(output)
-                time.sleep(.5)
             
-            ssh_client.close()
+            net_connect.disconnect()
 
-            return render_template('ssh.html', output='Successfully connected to ' + ip_address)
+            return render_template('ssh.html', output=f'Successfully connected to {ip_address}')
 
         except Exception as e:
             return render_template('ssh.html', alert=str(e))
     
     return render_template('ssh.html', output=None)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
