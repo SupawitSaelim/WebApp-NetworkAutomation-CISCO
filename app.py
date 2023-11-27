8f22e458f537a8f3930d4dd15523b6a7790563bc
Supawitsaelim <oatsupawit_159@outlook.com>
2023-11-27 23:28:34 +0700

    increase snmp

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import os
 from netmiko import ConnectHandler
 import paramiko
 import threading
+from flask import Flask, render_template, request, redirect, url_for
+from pysnmp.hlapi.asyncio.slim import Slim
+from pysnmp.hlapi import *
+from pysnmp.smi.rfc1902 import ObjectIdentity, ObjectType
+import asyncio
 
 app = Flask(__name__, template_folder='templates')
 
@@ -11,6 +16,10 @@ app.secret_key = 'Supawitadmin123_'
 
 json_file_path = 'cisco_device.json'
 cisco_devices = []
+
+port_status = {}
+port_oids = {}
+target_ip = ''
 
 if os.path.exists(json_file_path):
     with open(json_file_path, 'r') as f:
@@ -916,6 +925,164 @@ def is_ssh_reachable(ip, username, password):
         print(e)
         return False
 
+
+@app.route('/snmp', methods=['POST', 'GET'])
+def snmp():
+    return render_template('snmp.html', cisco_devices=cisco_devices , snmp_data = None , interface_data=None)
+
+
+def snmp_set(community_string, target_ip, interface_oid, new_status):
+    new_value = Integer(new_status)
+
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        setCmd(
+            SnmpEngine(),
+            CommunityData(community_string),
+            UdpTransportTarget((target_ip, 161)),
+            ContextData(),
+            ObjectType(ObjectIdentity(interface_oid), new_value)
+        )
+    )
+
+    if errorIndication:
+        print(f"SNMP Error: {errorIndication}")
+    else:
+        if errorStatus:
+            print(f"SNMP Error: {errorStatus.prettyPrint()}")
+        else:
+            print("Interface status has been set successfully")
+
+async def get_operational_status(target_ip, community_string, port_oid):
+    slim = Slim(1)
+    errorIndication, errorStatus, errorIndex, varBinds = await slim.get(
+        community_string,
+        target_ip,
+        161,
+        ObjectType(ObjectIdentity(port_oid))
+    )
+
+    if errorIndication:
+        return "Error"
+    elif errorStatus:
+        return "Error"
+    else:
+        return varBinds[0][1].prettyPrint()
+
+async def get_interface_data(target_ip, community_string):
+    slim = Slim(1)
+    interface_data = []
+
+    for port_number in range(1, 5):  
+        port_oid = "1.3.6.1.2.1.2.2.1.7." + str(port_number)
+        operational_status = await get_operational_status(target_ip, community_string, port_oid)
+        
+        interface_oid = "1.3.6.1.2.1.2.2.1.2." + str(port_number)
+        next_oid = ObjectType(ObjectIdentity(interface_oid))
+        errorIndication, errorStatus, errorIndex, varBinds = await slim.get(
+            community_string,
+            target_ip,
+            161,
+            next_oid
+        )
+        
+        if errorIndication:
+            return errorIndication
+        elif errorStatus:
+            return "{} at {}".format(
+                errorStatus.prettyPrint(),
+                errorIndex and varBinds[int(errorIndex) - 1][0] or "?",
+            )
+        else:
+            interface_name = varBinds[0][1].prettyPrint()
+            interface_data.append((interface_name, operational_status))
+            
+            port_status[interface_name] = operational_status
+            port_oids[interface_name] = port_oid
+
+    return interface_data
+
+async def get_snmp_data(target_ip, community_string):
+    slim = Slim(1)
+
+    errorIndication1, errorStatus1, errorIndex1, varBinds1 = await slim.get(
+        community_string,
+        target_ip,
+        161,
+        ObjectType(ObjectIdentity("SNMPv2-MIB", "sysUpTime", 0)),
+    )
+
+    errorIndication2, errorStatus2, errorIndex2, varBinds2 = await slim.get(
+        community_string,
+        target_ip,
+        161,
+        ObjectType(ObjectIdentity("SNMPv2-MIB", "sysDescr", 0)),
+    )
+    
+    errorIndication3, errorStatus3, errorIndex3, varBinds3 = await slim.get(
+        community_string,
+        target_ip,
+        161,
+        ObjectType(ObjectIdentity("SNMPv2-MIB", "sysName", 0)),
+    )
+
+    if errorIndication1 or errorIndication2 or errorIndication3:
+        return errorIndication1 or errorIndication2 or errorIndication3
+    elif errorStatus1 or errorStatus2 or errorStatus3:
+        return "{} at {}".format(
+            errorStatus1.prettyPrint(),
+            errorIndex1 and varBinds1[int(errorIndex1) - 1][0] or "?",
+        )
+    else:
+        sys_up_time = int(varBinds1[0][1])
+        sys_descr = varBinds2[0][1].prettyPrint()
+        sys_name = varBinds3[0][1].prettyPrint()
+        
+        seconds = sys_up_time // 100
+        minutes = seconds // 60
+        hours = minutes // 60
+
+        result = {
+            "SysUpTime": f"{hours:02}:{minutes % 60:02}:{seconds % 60:02}",
+            "SysDescr": sys_descr,
+            "SysName": sys_name
+        }
+        return result
+
+@app.route('/ip_address', methods=['GET', 'POST'])
+def get_ip_address():
+    if request.method == 'POST':
+        global target_ip
+        target_ip = request.form['ip_address']
+        community_string = 'public'  
+        snmp_data = asyncio.run(get_snmp_data(target_ip, community_string))
+        interface_data = asyncio.run(get_interface_data(target_ip, community_string))
+        return render_template('snmp.html', snmp_data=snmp_data, interface_data=interface_data)
+    return redirect(url_for('snmp'))
+
+@app.route('/control_port', methods=['POST'])
+def control_port():
+    if request.method == 'POST':
+        interface_name = request.form['interface_name']
+        action = request.form['action']
+        community_string = "private"
+        global target_ip
+
+        interface_name = request.form['interface_name']
+        original_oid = port_oids.get(interface_name, '')
+
+        modified_oid = original_oid.split('.')
+        modified_oid[-2] = '7'  # Assuming the digit to change is always the second-to-last
+        modified_oid = '.'.join(modified_oid)
+
+
+        new_status = 1 if action == 'on' else 2
+        
+        if modified_oid:    
+            snmp_set(community_string, target_ip,modified_oid , new_status)
+
+
+
+        return redirect(url_for('snmp'))
 
 
 if __name__ == '__main__':
